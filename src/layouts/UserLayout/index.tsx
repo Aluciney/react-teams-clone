0d8d2a1b284f6020584e327081d3a284e8c4a1e0
Aluciney Wanderley <aluciney.wanderley@gmail.com>
2021-07-16 16:34:25 -0400

    First Commit

---
+++ src/layouts/UserLayout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const UserLayout: React.FC = () => {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default UserLayout;
